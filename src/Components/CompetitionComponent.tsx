303688627141e7b6ab5337cacb21480c7dc04a99
tim <me@tgibbs.uk>
2023-08-06 19:57:40 +0100

    Limitations

--- src/Components/CompetitionComponent.tsx
+++ src/Components/CompetitionComponent.tsx
@@ -4,6 +4,7 @@ import { useCompetition } from "../Context/CompetitionContext";
 import { Regions } from "../Data/Regions";
 import { Region } from "../Types/Types";
 import AnglerComponent from "./AnglerComponent"
+import { LimitationsComponenet } from "./LimitationsComponenet";
 
 export const CompetitionComponent = () => {
     const x = useAnglerModal()
@@ -18,11 +19,14 @@ export const CompetitionComponent = () => {
         <Form.Select onChange={(e)=>{
                 var r = Regions.find(o=>o.letter === e.target.value);
                 if(r !== undefined) { onChangeRegion(r)}}}
-                value = {competition.region.letter}>
+                value = {competition.region.letter}
+                style={{ textAlign: "center" }} >
             {Regions.map((o,i)=><option key={o.letter} value={o.letter}>{o.name}</option>)}
-        </Form.Select>  
+        </Form.Select>
+        <LimitationsComponenet/>
         <h4>Anglers</h4>
         {competition.anglers.map((o,i)=><AnglerComponent key={'angler'+i} angler={o}/>)}
         <Button onClick={()=>x.showModal()}>+</Button>
     </Container>
 } 
+
